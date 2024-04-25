5aff0ca6babc0905dc935142f256b9380d766c4b
ChiWei-J <angeljan01@gmail.com>
2024-04-25 15:05:32 -0500

    Added github link

--- src/index.js
+++ src/index.js
@@ -3,15 +3,20 @@ import { createRoot } from "react-dom/client";
 
 import Weather from "./Weather";
 
-import "./Weather.css"
+import "./Weather.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-  <div className="App">
-    <Weather/>
+    <div className="App">
+      <Weather />
+      <footer>
+        <a href="https://github.com/ChiWei-J/weather-react-sandbox">
+          Coded by Chi Jan
+        </a>{" "}
+      </footer>
     </div>
   </StrictMode>
 );
